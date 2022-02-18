163a40b6ab77bc0742f5836766fc2e960d5d4fd6
Daniel Jimenez <barnique2802@gmail.com>
2022-02-18 17:37:19 -0300

    agregue getters y setters antes de saber si hay nativos en Js

---
+++ js/index.js
@@ -0,0 +1,66 @@
+class Tshirt {
+    constructor(talle, color, impresion_calidad, impresion_primaria, impresion_secundaria) {
+        this.size = talle;
+        this.color = color;
+        this.printing_quality = impresion_calidad;
+        this.printing_primary = impresion_primaria;
+        this.printing_secundary = impresion_secundaria; 
+    }
+
+    // // Getters and Setters
+
+    // getSize() {
+    //     return this.size;
+    // }
+
+    // setSize(talle){
+    //     this.size = talle;
+    // }
+
+    // getColor() {
+    //     return this.color;
+    // }
+
+    // setColor(color) {
+    //     this.color = color;
+    // }
+
+    // getPrinting_quality(){
+    //     return this.printing_quality;
+    // }
+
+    // setPrinting_quality(impresion_calidad){
+    //     this.printing_quality = impresion_calidad;
+    // }
+
+    // getPrinting_primary(){
+    //     return this.printing_primary;
+    // }
+    // setPrinting_primary(impresion_primaria){
+    //     this.printing_primary = impresion_primaria;
+    // }
+
+    // getPrinting_secundary(){
+    //     return this.printing_secundary;
+    // }
+    // setPrinting_secundary(impresion_secundaria){
+    //     this.printing_secundary = impresion_secundaria;
+    // }
+
+
+
+}
+
+/*  Precio Base $4.5
+    Colores no agregan nada
+    Talle
+        2xl en adelante vale $2.5 mas
+    Tipos de print
+        Direct to garment   ($4)   
+        screen print        (2$)    
+    Lugares de impresion
+        2 brazo
+        al frente
+        espalda
+    markup del 55%
+*/
