3055c65d8d9ff808990f4beb1dfd5213a1e967ef
Daniel Jimenez <barnique2802@gmail.com>
2022-02-19 04:11:23 -0300

    agregue el algoritmo basico de calculo de costo de una remera

--- js/index.js
+++ js/index.js
@@ -1,54 +1,92 @@
 class Tshirt {
-    constructor(talle, color, impresion_calidad, impresion_primaria, impresion_secundaria) {
-        this.size = talle;
-        this.color = color;
-        this.printing_quality = impresion_calidad;
-        this.printing_primary = impresion_primaria;
-        this.printing_secundary = impresion_secundaria; 
+    constructor(talle, impresion_calidad, impresion_primaria, impresion_secundaria) {
+        this.size = talle.toLowerCase(); 
+        this.printing_quality = impresion_calidad.toLowerCase();
+        this.printing_primary = impresion_primaria.toLowerCase();
+        this.printing_secundary = impresion_secundaria.toLowerCase(); 
+        this.basePrice = 4.5;
+        this.finalPrice = null;
     }
 
-    // // Getters and Setters
+    setPrice(){
 
-    // getSize() {
-    //     return this.size;
-    // }
+        let sum = 0;
+        switch(this.size){
+            case "s":
+            case "m":
+            case "l":
+            case "xl":
+                sum = this.basePrice;
+                break;
+            case "2xl":
+            case "3xl":
+            case "4xl":
+                sum = this.basePrice + 2.5;
+                break;
+            default:
+                alert("No se introdujo ninguna talla correcta");
+                return false;
+        }
 
-    // setSize(talle){
-    //     this.size = talle;
-    // }
+        switch(this.printing_quality){
+            case "direct to garment":
+                sum = sum + 4;
+                break;
+            case "screen print":
+                sum = sum + 2;
+                break;
+            default:
+                alert("No se introdujo un estilo de impresion valido");
+                return false;
+        }
 
-    // getColor() {
-    //     return this.color;
-    // }
+        switch(this.printing_primary){
+            case "front":
+            case "back":
+                sum = sum + 0;
+                break;
+            case "both":
+                if(this.printing_quality === "direct to garment"){
+                    sum = sum + 4;
+                }else if(this.printing_quality === "screen print"){
+                    sum = sum + 2;
+                }
+                break;
+            default:
+                alert("No se introdujo un lugar de impresion correcto")
+                return false;
+        }
 
-    // setColor(color) {
-    //     this.color = color;
-    // }
+        switch(this.printing_secundary){
+            case "right":
+            case "left":
+                if(this.printing_quality === "direct to garment"){
+                    sum = sum + 4;
+                }else if(this.printing_quality === "screen print"){
+                    sum = sum + 2;
+                }
+                break;
+            case "both":
+                if(this.printing_quality === "direct to garment"){
+                    sum = sum + 2*4;
+                }else if(this.printing_quality === "screen print"){
+                    sum = sum + 2*2;
+                }
+                break;
+            case "none":
+                sum = sum + 0;
+                break;
+            default:
+                alert("No se introdujo un lugar de impresion correcto")
+                return false;
+        }
 
-    // getPrinting_quality(){
-    //     return this.printing_quality;
-    // }
+        this.finalPrice = sum*1.55;
+    }
 
-    // setPrinting_quality(impresion_calidad){
-    //     this.printing_quality = impresion_calidad;
-    // }
-
-    // getPrinting_primary(){
-    //     return this.printing_primary;
-    // }
-    // setPrinting_primary(impresion_primaria){
-    //     this.printing_primary = impresion_primaria;
-    // }
-
-    // getPrinting_secundary(){
-    //     return this.printing_secundary;
-    // }
-    // setPrinting_secundary(impresion_secundaria){
-    //     this.printing_secundary = impresion_secundaria;
-    // }
-
-
-
+    getPrice(){
+        return this.finalPrice;
+    }
 }
 
 /*  Precio Base $4.5
@@ -64,3 +102,12 @@ class Tshirt {
         espalda
     markup del 55%
 */
+
+
+let talle = prompt("Introduzca la talla de la camisa que desea comprar: \n-s\n-m\n-l\n-xl\n-2xl\n-3xl\n-4xl");
+let impresion_calidad = prompt("Introduzca el tipo de impresion que desea:\n-Direct to garment\n-Screen print");
+let impresion_primaria =prompt("Desea la impresion en:\n-Front\n-Back\n-both");
+let impresion_secundaria =prompt("Desea la impresion en las mangas en:\n-right\n-left\n-both\n-none");
+const shirt = new Tshirt(talle,impresion_calidad,impresion_primaria,impresion_secundaria);
+shirt.setPrice();
+alert("El precio del articulo es " +shirt.finalPrice);
